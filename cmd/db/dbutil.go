0fe637fab2ad8ff6bd586e8aefbcb63fdcfe043a
zhangbing <sfwan520@outlook.com>
2024-03-19 16:45:05 +0800

    add:增加数据结构

--- cmd/db/dbutil.go
+++ cmd/db/dbutil.go
@@ -24,11 +24,8 @@ var DB *gorm.DB
 
 func InitDB() error {
 	var err error
-	DB, err = gorm.Open(sqlite.Open("wxboot.db"), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
+	DB, err = gorm.Open(sqlite.Open("wxbot.db"), &gorm.Config{Logger: logger.Default.LogMode(logger.Info)})
 	if err != nil {
-		return err
-	}
-	if err := DB.AutoMigrate(&Comment{}); err != nil {
 		return err
 	}
 	// 添加成功日志
@@ -36,39 +33,13 @@ func InitDB() error {
 	return nil
 }
 
-func CreateComment() {
-	comment := Comment{
-		WxID:        "test2",
-		WxNickName:  "test2",
-		Number:      2,
-		NovelTitle:  "test",
-		CommentText: "xxx",
-		CreateTime:  time.Now(),
-		UpdateTime:  time.Now(),
-	}
-
-	result := DB.Create(&comment) // 使用result变量来接收返回的结果
-
-	if result.Error != nil { // 检查是否有错误发生
+/*
+*
+插入评论信息
+*/
+func CreateComment(comment *Comment) {
+	result := DB.Create(comment) // 使用result变量来接收返回的结果
+	if result.Error != nil {     // 检查是否有错误发生
 		log.Printf("Error creating comment: %v\n", result.Error) // 记录错误
-	} else {
-		log.Printf("Comment created successfully: %v\n", comment) // 记录成功消息
 	}
-}
-
-func GetCommentByID(id uint) (*Comment, error) {
-	var comment Comment
-	result := DB.First(&comment, id)
-	if result.Error != nil {
-		return nil, result.Error
-	}
-	return &comment, nil
-}
-
-func UpdateCommentText(id uint, text string) error {
-	return DB.Model(&Comment{}).Where("id = ?", id).Update("comment", text).Error
-}
-
-func DeleteComment(id uint) error {
-	return DB.Delete(&Comment{}, id).Error
 }

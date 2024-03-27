6b4937d914f9eb8405914d27d39d477707cbd419
meow <njwzcb@163.com>
2024-03-27 21:17:04 +0800

    fix:fix bug

--- cmd/server/comentWeb.go
+++ cmd/server/comentWeb.go
@@ -1,14 +1,19 @@
 package server
 
 import (
+	"embed"
+	"io/fs"
 	"log"
 	"net/http"
 )
 
+//go:embed web/*
+var WebFlies embed.FS
+
 func StartWebServer() {
 	// 设置静态文件服务
-	fs := http.FileServer(http.Dir("./cmd/public"))
-	http.Handle("/", fs)
+	web, _ := fs.Sub(WebFlies, "web/")
+	http.Handle("/", http.FileServer(http.FS(web)))
 	// 启动服务器
 	log.Println("Starting web server on :8887")
 	if err := http.ListenAndServe(":8887", nil); err != nil {

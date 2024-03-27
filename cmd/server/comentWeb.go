f48ba6bab5466dec25a439ee982d2f7f427f1662
meow <njwzcb@163.com>
2024-03-27 19:56:02 +0800

    doc:调整结构

--- cmd/server/comentWeb.go
+++ cmd/server/comentWeb.go
@@ -7,11 +7,11 @@ import (
 
 func StartWebServer() {
 	// 设置静态文件服务
-	fs := http.FileServer(http.Dir("./public"))
+	fs := http.FileServer(http.Dir("./cmd/public"))
 	http.Handle("/", fs)
 	// 启动服务器
-	log.Println("Starting web server on :8081")
-	if err := http.ListenAndServe(":8081", nil); err != nil {
+	log.Println("Starting web server on :8887")
+	if err := http.ListenAndServe(":8887", nil); err != nil {
 		log.Fatalf("Failed to start web server: %v", err)
 	}
 }

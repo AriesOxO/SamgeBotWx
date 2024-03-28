8437046b2cde878c6e5bd4d2ace42659ffafa41a
zhangbing <sfwan520@outlook.com>
2024-03-28 10:07:50 +0800

    fix:解决资源路径问题

--- cmd/server/comentWeb.go
+++ cmd/server/comentWeb.go
@@ -12,8 +12,9 @@ var WebFlies embed.FS
 
 func StartWebServer() {
 	// 设置静态文件服务
-	web, _ := fs.Sub(WebFlies, "web/")
-	http.Handle("/", http.FileServer(http.FS(web)))
+	web, _ := fs.Sub(WebFlies, "web")
+	fs := http.FileServer(http.FS(web))
+	http.Handle("/", http.StripPrefix("/", fs))
 	// 启动服务器
 	log.Println("Starting web server on :8887")
 	if err := http.ListenAndServe(":8887", nil); err != nil {

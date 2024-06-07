8dd9364d31326cad80fad105a8e258ee1a3b4493
zhangbing <sfwan520@outlook.com>
2024-06-07 10:38:25 +0800

    fix：调整评论格式

--- cmd/wxBot/botHandler/botGroups.go
+++ cmd/wxBot/botHandler/botGroups.go
@@ -41,22 +41,29 @@ func FeiBang(ctx *openwechat.MessageContext) {
 			return
 		}
 		// 使用正则表达式解析消息
-		re := regexp.MustCompile(`@少爷\s(.+?)-评《(.+?)》(.+)`)
+		re := regexp.MustCompile(`(?s)@少爷\p{Z}+(.*?)\p{Z}+(.*?)\p{Z}+(.*)`)
 		matches := re.FindStringSubmatch(msgContent)
 		if len(matches) <= 2 {
-			ctx.ReplyText("评论格式错误，请参考格式[@少爷 笔名-评《小说名字》评价内容]，@" + sender.NickName)
+			ctx.ReplyText("评论格式错误，请参考格式[@少爷 笔名 小说名字 评价内容(字数需大于50)]，@" + sender.NickName)
 			return
 		}
-		if strutil.GetStrLength(matches[1]) < 1 {
-			ctx.ReplyText("未识别到笔名，请参考格式请参考格式[@少爷 笔名-评《小说名字》评价内容]，@" + sender.NickName)
+		if strutil.GetStrLength(matches[1]) < 1 && strutil.GetStrLength(matches[1]) > 10 {
+			ctx.ReplyText("未识别到笔名，请参考格式请参考格式[@少爷 笔名 小说名字 评价内容(字数需大于50)]，@" + sender.NickName)
 			return
 		}
 		if strutil.GetStrLength(matches[2]) < 1 {
-			ctx.ReplyText("未识别到小说名字，请参考格式请参考格式[@少爷 笔名-评《小说名字》评价内容]，@" + sender.NickName)
+			ctx.ReplyText("未识别到小说名字，请参考格式请参考格式[@少爷 笔名 小说名字 评价内容(字数需大于50)]，@" + sender.NickName)
 			return
 		}
-
-		if strutil.GetStrLength(matches[3]) < 30 {
+		if strings.Contains(matches[2], "《") && strings.Contains(matches[2], "》") {
+			ctx.ReplyText("格式错误，小说名字不能包含书名号@" + sender.NickName)
+			return
+		}
+		if strutil.GetStrLength(matches[2]) > 45 {
+			ctx.ReplyText("格式错误，小说名字过长，请检查格式是否错误，@" + sender.NickName)
+			return
+		}
+		if strutil.GetStrLength(matches[3]) < 50 {
 			ctx.ReplyText("评论内容过少，本少爷不收@" + sender.NickName)
 			return
 		}

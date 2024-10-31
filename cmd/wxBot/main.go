e58238ed25e5dbaf9059902737e00eb16d067f67
meow <ghost_meow@outlook.com>
2024-10-31 10:54:16 +0800

    reac：恢复注释

--- cmd/wxBot/main.go
+++ cmd/wxBot/main.go
@@ -6,6 +6,8 @@ import (
 	"SamgeWxApi/cmd/server"
 	"SamgeWxApi/cmd/server/db"
 	config "SamgeWxApi/cmd/utils/u_config"
+	"SamgeWxApi/cmd/wxBot/botHandler"
+	"SamgeWxApi/cmd/wxBot/botMsg"
 	"errors"
 	"fmt"
 )
@@ -16,18 +18,18 @@ func RunBot() {
 		panic(errors.New(fmt.Sprintf("InitCacheDir failed：%s", err.Error())))
 	}
 
-	//bot := botHandler.CreatBot()
-	//botMsg.ParseMessage(bot)
-	//if !botHandler.ParseLogin(bot) {
-	//	return
-	//}
-	//self, err := botHandler.ParseMine(bot)
-	//if err != nil {
-	//	return
-	//}
-	//botHandler.ParseFriends(self)
-	//botHandler.ParseGroups(self)
-	//_ = bot.Block() // 阻塞主goroutine, 直到发生异常或者用户主动退出
+	bot := botHandler.CreatBot()
+	botMsg.ParseMessage(bot)
+	if !botHandler.ParseLogin(bot) {
+		return
+	}
+	self, err := botHandler.ParseMine(bot)
+	if err != nil {
+		return
+	}
+	botHandler.ParseFriends(self)
+	botHandler.ParseGroups(self)
+	_ = bot.Block() // 阻塞主goroutine, 直到发生异常或者用户主动退出
 }
 func main() {
 	go db.InitDB()
